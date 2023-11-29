5e386e78bb8519849d7fc6eeea5212caada605de
Sanan Akhverdiev <120374801+sakhverdiev@users.noreply.github.com>
2023-11-29 13:06:51 +0400

    first commit

---
+++ Adapter-Builder-DesignPatterns/Adapter-DP/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+// Adapter => Oz sistemimize aid olmayan bir strukturu, Adapter vasitesile sanki oz sistemimize aidmis kimi gostere bilerik.
+
+
+namespace Adapter_DP;
+
+// Interface (sistemde isledilen struktur)
+interface IJsonSerializer
+{
+    public string SerializeObject(object obj);
+}
+
+// Movcud sisteme Impelement edilmek istenilen class
+class CustomSerializer
+{
+    public string Serialize(object obj)
+    {
+        // Gelen object serialize edilir...
+
+        return "Seialized with CustomSerializer";
+    }
+}
+
+// Adapter (Movcud sisteme Implement eden class)
+class CustomSeializerAdapter : IJsonSerializer
+{
+    public string SerializeObject(object obj)
+    {
+        CustomSerializer customSerializer = new CustomSerializer();
+        return customSerializer.Serialize(obj);
+    }
+}
+
+class CustomOperation
+{
+    private IJsonSerializer _jsonSerializer;
+
+    public CustomOperation(IJsonSerializer jsonSerializer)
+    {
+        _jsonSerializer = jsonSerializer;
+    }
+
+    public string Serialize(object obj)
+    {
+        return _jsonSerializer.SerializeObject(obj);
+    }
+}
+
+
+class Program
+{
+    public void Main()
+    {
+        var CustomOperation = new CustomOperation(new CustomSeializerAdapter());
+
+        string serializedObject = CustomOperation.Serialize(new object());
+        
+        Console.WriteLine(serializedObject);
+    }
+}
---
+++ Adapter-Builder-DesignPatterns/Builder-DP/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace Builder_DP;
+
+interface IBuilder
+{
+    void reset();
+    void setSeats(int number);
+    void setEngine(string engine);
+    void setTripComputer();
+    void setGPS();
+}
+
+
+class CarBuilder : IBuilder
+{
+
+    private Car car;
+
+    public void reset()
+    {
+        this.car = new Car();
+    }
+
+    public void setEngine(string engine) { }
+
+    public void setGPS() { }
+
+    public void setSeats(int number) { }
+
+    public void setTripComputer() { }
+
+    public Car getResult() { return this.car; }
+}
+
+
+class CarManualBuilder : IBuilder
+{
+
+    private Manual manual;
+
+    public void reset()
+    {
+        this.manual = new Manual();
+    }
+
+    public void setEngine(string engine) { }
+
+    public void setGPS() { }
+
+    public void setSeats(int number) { }
+
+    public void setTripComputer() { }
+
+    public Manual getResult()
+    {
+        return this.manual;
+    }
+}
+
+
+class Director
+{
+    public void makeSUV(IBuilder builder) { }
+
+    public void makeSportsCar(IBuilder builder)
+    {
+        builder.reset();
+        builder.setSeats(2);
+        builder.setEngine("Sports Car");
+        builder.setTripComputer();
+        builder.setGPS();
+    }
+}
+
+
+class Car : CarBuilder
+{
+    // ...
+}
+
+
+class Manual : CarManualBuilder
+{
+    // ...
+}
+
+
+// Client
+class Program
+{
+    public void Main()
+    {
+        Director director = new Director();
+        CarBuilder builder = new CarBuilder();
+        director.makeSportsCar(builder);
+        Car car = builder.getResult();
+    }
+}
